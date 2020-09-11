98466c6febb12123c37b3b6e764632bbafc069f5
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-09-11 09:01:38 +0530

    added menu item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
+import Homepage from "./pages/homepage/homepage";
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Homepage/>
   );
 }
 
